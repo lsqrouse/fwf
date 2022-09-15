3bb809aa6fd323e4f6e432736f9718f96209b164
Quinn Rouse <lsqrouse@gmail.com>
2022-09-15 18:40:12 -0400

    added basic database connectivity

--- api/server.js
+++ api/server.js
@@ -1,13 +1,73 @@
 const express = require("express");
+var Connection = require('tedious').Connection; 
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES; 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+var db_config = {  
+    server: 'fwf.database.windows.net',  //update me
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'fwf_admin', //update me
+            password: 'Us$&18gS2Yoz'  //update me
+        }
+    },
+    options: {
+        // If you are on Microsoft Azure, you need encryption:
+        encrypt: true,
+        database: 'fwf_dev'  //update me
+    }
+};  
+
+function executeStatement() {  
+    request = new Request("SELECT * FROM testing_table;", function(err) {  
+    if (err) {  
+        console.log(err);}  
+    });  
+    var result = "";  
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+          if (column.value === null) {  
+            console.log('NULL');  
+          } else {  
+            result+= column.value + " ";  
+          }  
+        });  
+        console.log(result);  
+        result ="";  
+    });  
+
+    request.on('done', function(rowCount, more) {  
+    console.log(rowCount + ' rows returned');  
+    });  
+    
+    // Close the connection after the final event emitted by the request, after the callback passes
+    request.on("requestCompleted", function (rowCount, more) {
+        connection.close();
+    });
+    connection.execSql(request);  
+}
+
+var connection = new Connection(db_config);  
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.
+    console.log("Connected, testing...");
+    executeStatement();  
+  
+});
+
+connection.connect();
+
+//basic testing endpoint
 app.get("/api", (req, res) => {
     res.json({message: "Hello world!"})
 });
 
+//starts the application
 app.listen(PORT, () => {
     console.log('Server listening on ' + PORT)
 });
