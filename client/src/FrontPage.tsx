25a38b49d19394048b2cb4644622f85164f66306
RobertHoensIII <60355422+RobertHoensIII@users.noreply.github.com>
2022-09-20 16:01:21 -0400

    Added API for lobbyID and userName

--- client/src/FrontPage.tsx
+++ client/src/FrontPage.tsx
@@ -1,6 +1,40 @@
 import React, { Component } from 'react';
 import './FrontPage.css';
+import { useState } from "react";
+
 export default class FrontPage extends Component {
+    state = {
+        userName: "",
+        lobbyID: 0
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          userName: this.state.userName,
+          lobbyID: this.state.lobbyID
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        this.setState({
+            userName: "",
+            lobbyID: 0,
+        })
+        
+      } else {
+        console.log("ERRRRRRRRRRRRRRR");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    }
+
+
     render() {
         return(
             <div className='container'>
@@ -18,8 +52,8 @@ export default class FrontPage extends Component {
                     
                     <button className='myButton'>Join</button>
                     <h1>Create Lobby</h1>
-                    <form action="/url" method="POST">
-                        <input type="text" placeholder="Username"/>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" placeholder="Username" onChange={(e) => this.setState({userName: e.target.value})}/>
                     </form>  
                     <button className='myButton'>Create</button>
                 </div>
@@ -30,3 +64,4 @@ export default class FrontPage extends Component {
         )
       }
 }
+
