22e8e06b3196d9739bbf62bf83499ee64b1714c5
Kartik Uppalapati <kartikuppalapati@Kartiks-MacBook-Air.local>
2022-09-29 18:20:14 -0400

    basic roles and user stories

--- api/mafia/src/Game.js
+++ api/mafia/src/Game.js
@@ -11,13 +11,72 @@ function assignRoles(allUsers, roleList) {
     // TODO: Randomly assign each role to a user. Need to create an instance of Player for each user,
     // then return all players as a list.
 
-    return;
+    return [];
 }
 
 function whoHasWon() {
     // TODO: Determine which team, if any, has won. If no one has one yet, return null.
 
     return null;
+}
+
+function doVote()
+{
+  // Initialize list of votes
+  let votesPerPlayer = {};
+
+  // Do voting with game system
+
+  // Get responses from user and update votes per player dict accordingly
+
+  // Get max and player name from votes per player
+  let max = 0;
+  let tie = 0;
+  let player = "";
+  for (const [key, value] of Object.entries(object)) 
+  {
+    // If current player has most votes then update max and player
+    if (value > max)
+    {
+      max = value;
+      player = key
+    }
+  }
+
+  // Send results to each player
+  sendToAllPlayers(
+    `<html>` + max.toString() + player.toString() + 
+    `<html>`
+  )
+}
+
+function getNightSummary()
+{
+  // Initialize empty night summary string
+  let nightSummary = ""
+
+  // Go through graveyard and find all dead players
+  for (let deadPlayer = 0; deadPlayer < this.graveyard.length; deadPlayer++)
+  {
+    // Add dead player name to summary list
+    nightSummary += "Oh no!" + deadPlayer.userId + "has died"
+  }
+
+  // Send night summary to each player
+  sendToAllPlayers(
+    `<html>` + nightSummary.toString() + 
+    `<html>`
+  )
+}
+
+function sendToAllPlayers(htmlData)
+{
+  // For loop to go through all players in player list
+  for (let player = 0; player < this.allPlayers.length; player++)
+  {
+    // Send html data to each player
+    print(player)
+  }
 }
 
 /**
