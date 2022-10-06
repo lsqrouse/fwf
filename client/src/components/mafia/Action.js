4b5a27162a49ee568ea7545a69582c6b7987d210
Brian Yang <yang1839@purdue.edu>
2022-10-06 11:39:08 -0400

    action.js

---
+++ client/src/components/mafia/Action.js
@@ -0,0 +1,17 @@
+function Action(props) {
+    return(
+        <div className='Action'>
+            
+            <RoleList rolelist={props.roleList}/>
+        </div>
+    );
+}
+
+function RoleList(props) {
+    const roleList = props.roleList;
+
+    
+}
+
+export default Action;
+
