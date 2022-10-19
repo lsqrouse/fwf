c8b5c584c99689ebc4adbd66a3813083b425d180
Jamie U <20melons@gmail.com>
2022-10-18 22:01:17 -0400

    Check unbalanced and invalid roles

--- client/src/components/mafia/MafiaContainer.js
+++ client/src/components/mafia/MafiaContainer.js
@@ -2,18 +2,6 @@ import { useState } from "react";
 import Settings from './Settings';
 import InGame from './InGame';
 import { useSelector } from "react-redux";
-
-import PlayerIcon from "../../images/mafia/icons/player.png";
-import VillagerIcon from "../../images/mafia/icons/villager.png";
-import DetectiveIcon from "../../images/mafia/icons/detective.png";
-import DoctorIcon from "../../images/mafia/icons/doctor.png";
-import VigilanteIcon from "../../images/mafia/icons/vigilante.png"
-import DrunkIcon from "../../images/mafia/icons/drunk.png"
-import MafiaIcon from "../../images/mafia/icons/mafia.png";
-import DistractorIcon from "../../images/mafia/icons/distractor.png"
-import ExecutionerIcon from "../../images/mafia/icons/executioner.png";
-import FramerIcon from "../../images/mafia/icons/framer.png";
-import RessurectionistIcon from "../../images/mafia/icons/ressurectionist.png";
 import roles from "../../data/mafia/roles";
 
 function MafiaContainer(props) {
@@ -22,6 +10,7 @@ function MafiaContainer(props) {
   const gameScreen = useSelector((state) => state.lobbyState.gameScreen);
   const [socket, setSocket] = useState(props.socket);
   const lobbyState = useSelector((state) => state.lobbyState);
+  const [warnMessage, setWarnMessage] = useState("");
   const minPlayers = 4;
 
   function startGame() {
@@ -29,29 +18,75 @@ function MafiaContainer(props) {
     //setNumPlayers(lobbyState.playerList.length);
     console.log(lobbyState.playerList.length);
     console.log("NUM PLAYERS %d", numPlayers);
-    const playersNow = lobbyState.playerList.length;
-    if(playersNow < minPlayers) {
-      alert("not enough players in game");
-    }else if (playersNow > selectedRoles.length) {
-      alert("Not enough roles selected!");
-    } else if (playersNow < selectedRoles.length) {
-      alert("Too many roles selected!");
+    if (numPlayers < minPlayers) {
+      setWarnMessage("Need at least " + minPlayers + " players.");
     } else {
-        //setGameScreen("Game")
-      console.log("starting game socketemit")
-      var startData = {
-        lobbyId: lobbyState.lobbyId,
-        selectedRoles: selectedRoles,
+      const check = checkRoleList();
+      if (!check.valid) {
+        setWarnMessage(check.message);
+      } else {
+        if (check.message === "OK" || (warnMessage !== "")) {
+          console.log("starting game socketemit");
+          var startData = {
+            lobbyId: lobbyState.lobbyId,
+            selectedRoles: selectedRoles,
+          }
+          socket.emit("start_game", startData);
+        } else {
+          setWarnMessage(check.message + " Game can still be started.");
+        }
       }
-      socket.emit("start_game", startData)
     }
+  }
 
+  function checkRoleList() {
+    let mafia = 0;
+    let village = 0;
+    let other = 0;
+    for (let i = 0; i < selectedRoles.length; i++) {
+      const roleName = selectedRoles[i];
+      switch (getTeam(roleName)) {
+        case "Mafia":
+          mafia++;
+          break
+        case "Village":
+          village++;
+          break
+        default:
+          other++;
+      }
+    }
+    if (selectedRoles.length > numPlayers) {
+      return {message: "Not enough roles selected!", valid: false};
+    }
+    if (selectedRoles.length < numPlayers) {
+      return {message: "Too few roles selected!", valid: false};
+    }
+    if (village === 0) {
+      return {message: "At least one Village role must be selected.", valid: false};
+    }
+    if (mafia === 0) {
+      return {message: "At least one Mafia role must be selected.", valid: false};
+    }
+    if (mafia >= village - 1) {
+      return {message: "It is recommended to select more Village roles!", valid: true};
+    }
+    if (numPlayers > 8 && mafia < 3) {
+      return {message: "It is recommended to select more Mafia roles!", valid: true};
+    }
+    return {message: "OK", valid: true};
+  }
+
+  function getTeam(roleName) {
+    const role = roles.find(r => r.name === roleName);
+    return role.team;
   }
 
   function updateSelectedRoles(roles) {
     const newSelectedRoles = roles;
     lobbyState.settings.selectedRoles = newSelectedRoles;
     socket.emit("update_lobby_state", lobbyState);
+    setWarnMessage("");
   }
 
   function endGame() {
@@ -74,6 +109,7 @@ function MafiaContainer(props) {
           setSelectedRoles={updateSelectedRoles}
           startGame={startGame}
           endGame={endGame}
+          warnMessage={warnMessage}
           socket={socket}
         />)
     }
@@ -89,6 +125,7 @@ function SettingsScreen(props) {
   const startGame = props.startGame;
   const endGame = props.endGame;
   const isHost = useSelector((state) => state.playerState.host);
+  const warnMessage = props.warnMessage;
   const socket = props.socket;
 
   return (
@@ -108,6 +145,7 @@ function SettingsScreen(props) {
       <div>
         <button type="button" class="endGameButton" onClick={endGame}>End Game</button>
       </div>
+      <div id="warnMessage">{warnMessage}</div>
       </>}
     </>
   );
