898ec082cd5713fc064942eaca0563362d4312aa
Brian Yang <yang1839@purdue.edu>
2022-11-04 10:33:44 -0400

    Update InGame.js

--- client/src/components/werewolf/InGame.js
+++ client/src/components/werewolf/InGame.js
@@ -1,3 +1,12 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
+function inGame(props) {
+    const playerRole = useSelector((state) => state.playerState.gamePlayerState.role);
+    return (
+        <div>
+            <RoleCard role={roles[playerRole]} />
+        </div>
+    );
+}
+export default inGame;
