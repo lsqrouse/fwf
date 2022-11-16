c48fd5984378636ba4a63c1f178b3b6b05ac963c
Jamie U <20melons@gmail.com>
2022-11-16 00:10:09 -0500

    TimerSettings layout

--- client/src/components/mafia/TimerSettings.js
+++ client/src/components/mafia/TimerSettings.js
@@ -2,19 +2,22 @@ function TimerSettings(props) {
 
   return (
     <div className="timerSettings">
-      <input type="text" name="frameName" className="timeInput mafiaInput1"></input>
-        <button className="mafiaButton2" onClick={() => {
-          var fname = document.getElementById("dayTime");
-          var time = fname.value;
-          //handleDayPhaseTime(time);
-        }}>Set day phase time in seconds </button>
-
-        <input type="text" name="frameName" className="timeInput mafiaInput1"></input>
-        <button className="mafiaButton2" onClick={() => {
-          var fname = document.getElementById("nightPhaseTime");
-          var time = fname.value;
-          //handleNightPhaseTime(time);
-        }}>Set night phase time in seconds </button>
+      <p>
+        Optionally set time limits for the day and night time phases. Setting to 0 or lower puts no limit.
+      </p>
+      <input type="text" id="dayPhaseTimeInput" className="timeInput mafiaInput1"></input>
+      <button className="mafiaButton2" onClick={() => {
+        var fname = document.getElementById("dayPhaseTimeInput");
+        var time = fname.value;
+        //handleDayPhaseTime(time);
+      }}>Set day phase time in seconds </button>
+      <br />
+      <input type="text" id="nightPhaseTimeInput" className="timeInput mafiaInput1"></input>
+      <button className="mafiaButton2" onClick={() => {
+        var fname = document.getElementById("nightPhaseTimeInput");
+        var time = fname.value;
+        //handleNightPhaseTime(time);
+      }}>Set night phase time in seconds </button>
     </div>
   );
 }
