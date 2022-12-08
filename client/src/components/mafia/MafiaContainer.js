9daf66e15f94fa11301cf76449d1ec8858ee8739
Jamie U <20melons@gmail.com>
2022-12-08 16:13:30 -0500

    Display summary of game and winning playres

--- client/src/components/mafia/MafiaContainer.js
+++ client/src/components/mafia/MafiaContainer.js
@@ -3,6 +3,7 @@ import Settings from './Settings';
 import InGame from './InGame';
 import { useSelector } from "react-redux";
 import "../../styles/mafia/reusable.css";
+import { getIcon } from "./getIcon";
 
 function MafiaContainer(props) {
   const numPlayers = useSelector((state) => state.lobbyState.playerList).length;;
@@ -184,14 +185,109 @@ function EndGameScreen(props) {
   const gameState = useSelector((state) => state.lobbyState.gameState);
 
   return (
-    <>
+    <div className="endGame">
       <h2>
         {gameState.winningTeams.map((team, index) => 
           team + (index < gameState.winningTeams.length - 1 ? " and " : "")
         )} has won!
       </h2>
-    </>
+      <div className="endGameInfo">
+        <WinningPlayers />
+        <GameSummary />
+      </div>
+    </div>
   )
+}
+
+function WinningPlayers() {
+  const gameState = useSelector((state) => state.lobbyState.gameState);
+
+  return (
+  <div className="winningPlayersDiv">
+    <h3>Winners:</h3>
+    <ul className="winningPlayersList">
+      {gameState.winningPlayers.map(player =>
+        <li key={player.id}>
+          <img src={getIcon(player.gamePlayerState.role)} width="25px" alt={player.gamePlayerState.role}/>
+          {player.nickname}
+        </li>
+      )}
+    </ul>
+  </div>
+  );
+}
+
+function GameSummary() {
+  const lobbyState = useSelector((state) => state.lobbyState);
+  const history = lobbyState.gameState.history;
+
+  function getPlayerFromId(id) {
+    return lobbyState.playerList.find(player => player.id === id);
+  }
+
+  function DaySummary(props) {
+    const phaseNum = props.phaseNum;
+    const data = props.data;
+
+    return (
+      <div>
+        <h6>Day {phaseNum}</h6>
+        <ul>
+          <li key="dayVote">
+            <b>{data.dayVote ? getPlayerFromId(data.dayVote).nickname : "No one"}</b> was voted out.
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  function NightSummary(props) {
+    const phaseNum = props.phaseNum;
+    const data = props.data;
+
+    return (
+      <div>
+        <h6>Night {phaseNum}</h6>
+        <ul>
+          {Object.keys(data.night).map(playerId =>
+            data.night[playerId].ability !== "ok" &&
+            <li key={playerId}>
+              <b>{getPlayerFromId(playerId).nickname}</b>
+              &nbsp;went out to&nbsp;
+              {data.night[playerId].ability}&nbsp;
+              {data.night[playerId].targets.map((target, index) =>
+                <><b>{getPlayerFromId(target).nickname}</b>{index < data.night[playerId].targets.length - 1 ? ", " : "."}</>
+              )}
+            </li>
+          )}
+          
+          {data.mafiaKill &&
+            <li key="mafiaKill">
+              The mafia went out to kill <b>{getPlayerFromId(data.mafiaKill).nickname}</b>.
+            </li>
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  function SummaryList() {
+   return (
+    Object.keys(history).map(phaseNum =>
+      <>
+        <NightSummary phaseNum={phaseNum} data={history[phaseNum]} />
+        {history[phaseNum].hasOwnProperty("dayVote") && <DaySummary phaseNum={phaseNum} data={history[phaseNum]} />}
+      </>
+    )
+   );
+  }
+
+  return (
+  <div className="gameSummaryDiv">
+    <h3>Summary:</h3>
+    <SummaryList />
+  </div>
+  );
 }
 
 export default MafiaContainer;
